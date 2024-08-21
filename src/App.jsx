007a8f01f3b5a8f7ac0812ac654a835c92c64911
naol <naolmeseret22@gmail.com>
2024-08-21 16:33:19 +0300

    i m done with everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,45 @@
+import { Nav } from "./components";
 import {
+  CustomerReviews,
+  Footer,
   Hero,
   PopularProducts,
-  Footer,
-  Spacialoffer,
   Services,
-  Subscibe,
-  SuperQuality, 
-  CustomerReviews
-} from './sections'
-import Nav from'./componets/Nav'
-const App=()=>(
-  <main className="relative">
-     <Nav /> 
-    <section className="xl:padding-1 wide:padding-r padding-b">
-    <Hero />
-    </section>
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
 
-    <section className="padding">
-<PopularProducts />
-    </section>
+const App = () => {
+  return (
+    <main className='relative'>
+      <Nav />
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero />
+      </section>
+      <section className='padding'>
+        <PopularProducts />
+      </section>
+      <section className='padding'>
+        <SuperQuality />
+      </section>
+      <section className='padding-x py-10'>
+        <Services />
+      </section>
+      <section className='padding'>
+        <SpecialOffer />
+      </section>
+      <section className='bg-pale-blue padding'>
+        <CustomerReviews />
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <Subscribe />
+      </section>
+      <section className=' bg-black padding-x padding-t pb-8'>
+        <Footer />
+      </section>
+    </main>
+  );
+};
 
-  <section className="padding"> 
- < SuperQuality /> 
-  </section>
-  <section className="padding-x py-10">
-  < Services />
-  </section>
-
-  <section className="padding">
-   <Spacialoffer />
-  </section>
-  <section className="bg-pale-blue padding">
-   <CustomerReviews />
-  </section>
-  <section className="padding-x sm:py-32 px-16 w-full">
-   <Subscibe />
-  </section>
-  <section className="bg-black padding-x padding-t pb-8">
-   <Footer />
-  </section>
-
-  </main>
-
-);
 export default App;
